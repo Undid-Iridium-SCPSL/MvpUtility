a5420794c5b0828164a5a1f76cf0036c73f38a7f
undid-iridium <francis.x.irizarry@gmail.com>
2022-05-02 12:24:33 -0400

    Version update.

--- MvpUtility/Main.cs
+++ MvpUtility/Main.cs
@@ -29,7 +29,7 @@
         public override Version RequiredExiledVersion { get; } = new Version(5, 1, 3);
 
         /// <inheritdoc />
-        public override Version Version { get; } = new Version(1, 1, 0);
+        public override Version Version { get; } = new Version(1, 1, 1);
 
         /// <summary>
         /// Gets an instance of the <see cref="MvpStats"/> class.
