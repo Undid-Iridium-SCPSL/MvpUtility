cbe09b41678e77ea1f282b8fe994527a5422eeae
undid-iridium <francis.x.irizarry@gmail.com>
2022-05-10 21:41:55 -0400

    Forgot to update version.

--- MvpUtility/Main.cs
+++ MvpUtility/Main.cs
@@ -36,7 +36,7 @@ namespace MvpUtility
         public override Version RequiredExiledVersion { get; } = new Version(5, 1, 3);
 
         /// <inheritdoc />
-        public override Version Version { get; } = new Version(1, 1, 1);
+        public override Version Version { get; } = new Version(1, 1, 2);
 
         /// <summary>
         /// Gets an instance of the <see cref="MvpStats"/> class.
